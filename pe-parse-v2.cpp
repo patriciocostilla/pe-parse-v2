ba2fc465a935fc41343d7a44997af6783da9486a
Juan Patricio COSTILLA <patriciocostilla@gmail.com>
2020-10-21 21:09:20 -0300

    printing report in markdown format

--- pe-parse-v2.cpp
+++ pe-parse-v2.cpp
@@ -67,13 +67,13 @@ int main(int argc, char* argv[]) {
     // Get Export Section
     int exportSectionRva = nth->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress;
     IMAGE_SECTION_HEADER* exportSection = getSection(fileData, sectionHeadersOffset, numberOfSections, exportSectionRva);
-    printf("Export section at %s (%x)\n", exportSection->Name, exportSection->Misc.PhysicalAddress);
+    printf("Export section at %s (%#0x)\n", exportSection->Name, exportSection->Misc.PhysicalAddress);
     parseExportDirectory(fileData, exportSection, exportSectionRva);
 
     // Parse Import Directory
     int importSectionRva = nth->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress;
     IMAGE_SECTION_HEADER* importSection = getSection(fileData, sectionHeadersOffset, numberOfSections, importSectionRva);
-    printf("Import section at %s (%x)\n", importSection->Name, importSection->Misc.PhysicalAddress);
+    printf("Import section at %s (%#0x)\n", importSection->Name, importSection->Misc.PhysicalAddress);
     parseImportDirectory(fileData, importSection, importSectionRva);
 
     return 0;
@@ -95,13 +95,13 @@ char* getFileName(int argc, char* argv[]) {
 }
 
 FILE* openFile(char fileName[]) {
-    printf("\nOpening file: %s\n", fileName);
+    printf("\n* Opening file: %s\n", fileName);
     FILE* fp = fopen(fileName, "r");
     if (fp == NULL) {
-        printf("\nFile Open error. Aborting\n");
+        printf("\n\t* File Open error. Aborting\n");
         exit(0);
     }
-    printf("\nFile opened: %s (%x)\n", fileName, (int)fp);
+    printf("\n\t* Success! file %s opened at: %#0x\n", fileName, (int)fp);
     return fp;
 }
 
@@ -120,17 +120,18 @@ int getFileSize(FILE *fp) {
     fseek(fp, 0, SEEK_END);
     // Calculte file size
     long int size = ftell(fp);
-    printf("File size is %d bytes\n", size);
+    printf("\n\t\t* File size is %d bytes\n", size);
     // Reset file pointer
     rewind(fp);
     return size;
 }
 
 char* readFullFile(FILE* fp, int size) {
-    printf("Reading file data to memory\n");
+    printf("\n\t\t* Reading file data to memory\n");
     char* fileData = (char*)malloc(size);
     int readed = fread(fileData, 1, size, fp);
-    printf("File data readed to %x\n", (int)fileData);
+    printf("\n\t\t\t* Success! File data readed to %#0x\n", (int)fileData);
+    printf("\n\t\t\t* Parsing...\n");
     return fileData;
 }
 
@@ -138,26 +139,28 @@ IMAGE_DOS_HEADER* parseDosHeader(char* fileData) {
     IMAGE_DOS_HEADER* dh = (IMAGE_DOS_HEADER*)malloc(sizeof(IMAGE_DOS_HEADER));
     dh = (IMAGE_DOS_HEADER*)fileData;
     // Printing DOS-HEADER
-    printf("[DOS-HEADER]\n");
-    printf("e_magic: %x\n", dh->e_magic);
-    printf("e_cblp: %x\n", dh->e_cblp);
-    printf("e_cp: %x\n", dh->e_cp);
-    printf("e_crlc: %x\n", dh->e_crlc);
-    printf("e_cparhdr: %x\n", dh->e_cparhdr);
-    printf("e_minalloc: %x\n", dh->e_minalloc);
-    printf("e_maxalloc: %x\n", dh->e_maxalloc);
-    printf("e_ss: %x\n", dh->e_ss);
-    printf("e_sp: %x\n", dh->e_sp);
-    printf("e_csum: %x\n", dh->e_csum);
-    printf("e_ip: %x\n", dh->e_ip);
-    printf("e_cs: %x\n", dh->e_cs);
-    printf("e_lfarlc: %x\n", dh->e_lfarlc);
-    printf("e_ovno: %x\n", dh->e_ovno);
-    printf("e_res: %x\n", *dh->e_res); //array
-    printf("e_oemid: %x\n", dh->e_oemid);
-    printf("e_oeminfo: %x\n", dh->e_oeminfo);
-    printf("e_res2: %x\n", *dh->e_res2); //array
-    printf("e_lfanew: %x\n", dh->e_lfanew);
+    printf("\n## DOS-HEADER\n");
+    printf("\n| DOS HEADER | |\n");
+    printf("| ------- | --: |\n");
+    printf("e_magic | %#0x \n", dh->e_magic);
+    printf("e_cblp | %#0x \n", dh->e_cblp);
+    printf("e_cp | %#0x \n", dh->e_cp);
+    printf("e_crlc | %#0x \n", dh->e_crlc);
+    printf("e_cparhdr | %#0x \n", dh->e_cparhdr);
+    printf("e_minalloc | %#0x \n", dh->e_minalloc);
+    printf("e_maxalloc | %#0x \n", dh->e_maxalloc);
+    printf("e_ss | %#0x \n", dh->e_ss);
+    printf("e_sp | %#0x \n", dh->e_sp);
+    printf("e_csum | %#0x \n", dh->e_csum);
+    printf("e_ip | %#0x \n", dh->e_ip);
+    printf("e_cs | %#0x \n", dh->e_cs);
+    printf("e_lfarlc | %#0x \n", dh->e_lfarlc);
+    printf("e_ovno | %#0x \n", dh->e_ovno);
+    printf("e_res | %#0x \n", *dh->e_res); //array
+    printf("e_oemid | %#0x \n", dh->e_oemid);
+    printf("e_oeminfo | %#0x \n", dh->e_oeminfo);
+    printf("e_res2 | %#0x \n", *dh->e_res2); //array
+    printf("e_lfanew | %#0x \n", dh->e_lfanew);
     printf("\n");
     return dh;
 }
@@ -165,8 +168,10 @@ IMAGE_DOS_HEADER* parseDosHeader(char* fileData) {
 IMAGE_NT_HEADERS32  * parseNtHeaders(char* fileData, int ntHeaderOffset) {
     IMAGE_NT_HEADERS32  * nth = (IMAGE_NT_HEADERS32  *)malloc(sizeof(IMAGE_NT_HEADERS32));
     nth = (IMAGE_NT_HEADERS32  *) &fileData[ntHeaderOffset];
-    printf("[NT-HEADERS]\n");
-    printf("Signature: %x\n", nth->Signature);
+    printf("\n## NT-HEADERS\n");
+    printf("\n| NT-HEADERS | |\n");
+    printf("| --------- | --: |\n");
+    printf(" Signature | %#0x\n", nth->Signature);
     printf("\n");
     return nth;
 }
@@ -175,87 +180,95 @@ IMAGE_FILE_HEADER* parseFileHeader(char* fileData, int fileHeaderOffset) {
     IMAGE_FILE_HEADER* fh = (IMAGE_FILE_HEADER*)malloc(sizeof(IMAGE_FILE_HEADER));
     fh = (IMAGE_FILE_HEADER*)&fileData[fileHeaderOffset];
     printf("[FILE-HEADER]\n");
-    printf("machine: %x\n", fh->Machine);
-    printf("number_of_sections: %x\n", fh->NumberOfSections);
-    printf("time_date_stamp: %x\n", fh->TimeDateStamp);
-    printf("pointer_to_symbol_table: %x\n", fh->PointerToSymbolTable);
-    printf("number_of_symbols: %x\n", fh->NumberOfSymbols);
-    printf("size_of_optional_header: %x\n", fh->SizeOfOptionalHeader);
-    printf("characteristics: %x\n", fh->Characteristics);
+    printf("machine: %#0x\n", fh->Machine);
+    printf("number_of_sections: %#0x\n", fh->NumberOfSections);
+    printf("time_date_stamp: %#0x\n", fh->TimeDateStamp);
+    printf("pointer_to_symbol_table: %#0x\n", fh->PointerToSymbolTable);
+    printf("number_of_symbols: %#0x\n", fh->NumberOfSymbols);
+    printf("size_of_optional_header: %#0x\n", fh->SizeOfOptionalHeader);
+    printf("characteristics: %#0x\n", fh->Characteristics);
     return fh;
 }
 */
 
 void printFileHeader(IMAGE_FILE_HEADER fh) {
-    printf("[FILE-HEADER]\n");
-    printf("machine: %x\n", fh.Machine);
-    printf("number_of_sections: %x\n", fh.NumberOfSections);
-    printf("time_date_stamp: %x\n", fh.TimeDateStamp);
-    printf("pointer_to_symbol_table: %x\n", fh.PointerToSymbolTable);
-    printf("number_of_symbols: %x\n", fh.NumberOfSymbols);
-    printf("size_of_optional_header: %x\n", fh.SizeOfOptionalHeader);
-    printf("characteristics: %x\n", fh.Characteristics);
+    printf("\n| FILE-HEADER | |\n");
+    printf("| --------- | --: |\n");
+    printf("machine | %#0x\n", fh.Machine);
+    printf("number_of_sections | %#0x\n", fh.NumberOfSections);
+    printf("time_date_stamp | %#0x\n", fh.TimeDateStamp);
+    printf("pointer_to_symbol_table | %#0x\n", fh.PointerToSymbolTable);
+    printf("number_of_symbols | %#0x\n", fh.NumberOfSymbols);
+    printf("size_of_optional_header | %#0x\n", fh.SizeOfOptionalHeader);
+    printf("characteristics | %#0x\n", fh.Characteristics);
 }
 
 void printOptionalHeader(IMAGE_OPTIONAL_HEADER32 oh) {
-    printf("[OPTIONAL-HEADER]");
-    printf("Magic: %x\n", oh.Magic);
-    printf("MajorLinkerVersion: %x\n", oh.MajorLinkerVersion);
-    printf("MinorLinkerVersion: %x\n", oh.MinorLinkerVersion);
-    printf("SizeOfCode: %x\n", oh.SizeOfCode);
-    printf("SizeOfInitializedData: %x\n", oh.SizeOfInitializedData);
-    printf("SizeOfUninitializedData: %x\n", oh.SizeOfUninitializedData);
-    printf("AddressOfEntryPoint: %x\n", oh.AddressOfEntryPoint);
-    printf("BaseOfCode: %x\n", oh.BaseOfCode);
-    printf("BaseOfData: %x\n", oh.BaseOfData);
-    printf("ImageBase: %x\n", oh.ImageBase);
-    printf("SectionAlignment: %x\n", oh.SectionAlignment);
-    printf("FileAlignment: %x\n", oh.FileAlignment);
-    printf("MajorOperatingSystemVersion: %x\n", oh.MajorOperatingSystemVersion);
-    printf("MinorOperatingSystemVersion: %x\n", oh.MinorOperatingSystemVersion);
-    printf("MajorImageVersion: %x\n", oh.MajorImageVersion);
-    printf("MinorImageVersion: %x\n", oh.MinorImageVersion);
-    printf("MajorSubsystemVersion: %x\n", oh.MajorSubsystemVersion);
-    printf("MinorSubsystemVersion: %x\n", oh.MinorSubsystemVersion);
-    printf("Win32VersionValue: %x\n", oh.Win32VersionValue);
-    printf("SizeOfImage: %x\n", oh.SizeOfImage);
-    printf("SizeOfHeaders: %x\n", oh.SizeOfHeaders);
-    printf("CheckSum: %x\n", oh.CheckSum);
-    printf("Subsystem: %x\n", oh.Subsystem);
-    printf("DllCharacteristics: %x\n", oh.DllCharacteristics);
-    printf("SizeOfStackReserve: %x\n", oh.SizeOfStackReserve);
-    printf("SizeOfStackCommit: %x\n", oh.SizeOfStackCommit);
-    printf("SizeOfHeapReserve: %x\n", oh.SizeOfHeapReserve);
-    printf("SizeOfHeapCommit: %x\n", oh.SizeOfHeapCommit);
-    printf("LoaderFlags: %x\n", oh.LoaderFlags);
-    printf("NumberOfRvaAndSizes: %x\n", oh.NumberOfRvaAndSizes);
+    printf("\n| OPTIONAL-HEADER | |\n");
+    printf("| --------- | --: |\n");
+    printf("Magic | %#0x\n", oh.Magic);
+    printf("MajorLinkerVersion | %#0x\n", oh.MajorLinkerVersion);
+    printf("MinorLinkerVersion | %#0x\n", oh.MinorLinkerVersion);
+    printf("SizeOfCode | %#0x\n", oh.SizeOfCode);
+    printf("SizeOfInitializedData | %#0x\n", oh.SizeOfInitializedData);
+    printf("SizeOfUninitializedData | %#0x\n", oh.SizeOfUninitializedData);
+    printf("AddressOfEntryPoint | %#0x\n", oh.AddressOfEntryPoint);
+    printf("BaseOfCode | %#0x\n", oh.BaseOfCode);
+    printf("BaseOfData | %#0x\n", oh.BaseOfData);
+    printf("ImageBase | %#0x\n", oh.ImageBase);
+    printf("SectionAlignment | %#0x\n", oh.SectionAlignment);
+    printf("FileAlignment | %#0x\n", oh.FileAlignment);
+    printf("MajorOperatingSystemVersion | %#0x\n", oh.MajorOperatingSystemVersion);
+    printf("MinorOperatingSystemVersion | %#0x\n", oh.MinorOperatingSystemVersion);
+    printf("MajorImageVersion | %#0x\n", oh.MajorImageVersion);
+    printf("MinorImageVersion | %#0x\n", oh.MinorImageVersion);
+    printf("MajorSubsystemVersion | %#0x\n", oh.MajorSubsystemVersion);
+    printf("MinorSubsystemVersion | %#0x\n", oh.MinorSubsystemVersion);
+    printf("Win32VersionValue | %#0x\n", oh.Win32VersionValue);
+    printf("SizeOfImage | %#0x\n", oh.SizeOfImage);
+    printf("SizeOfHeaders | %#0x\n", oh.SizeOfHeaders);
+    printf("CheckSum | %#0x\n", oh.CheckSum);
+    printf("Subsystem | %#0x\n", oh.Subsystem);
+    printf("DllCharacteristics | %#0x\n", oh.DllCharacteristics);
+    printf("SizeOfStackReserve | %#0x\n", oh.SizeOfStackReserve);
+    printf("SizeOfStackCommit | %#0x\n", oh.SizeOfStackCommit);
+    printf("SizeOfHeapReserve | %#0x\n", oh.SizeOfHeapReserve);
+    printf("SizeOfHeapCommit | %#0x\n", oh.SizeOfHeapCommit);
+    printf("LoaderFlags | %#0x\n", oh.LoaderFlags);
+    printf("NumberOfRvaAndSizes | %#0x\n", oh.NumberOfRvaAndSizes);
 
-    printf("[OPTIONAL-HEADER->data-directory]\n");
+    printf("\n### DATA-DIRECTORIES\n");
+    printf("\n| N | Virtual Address | Size |\n");
+    printf("| --------- | -- | -- |\n");
     for (int i = 0; i < IMAGE_NUMBEROF_DIRECTORY_ENTRIES; i++) {
         IMAGE_DATA_DIRECTORY idd = oh.DataDirectory[i];
+        printf(" %d | %#0x | %#0x\n", i, idd.VirtualAddress, idd.Size);
+        /*
         printf("\t[%d]\n", i);
-        printf("\t - VirtualAddress: %x\n", idd.VirtualAddress);
-        printf("\t - Size: %x\n", idd.Size);
+        printf("\t - VirtualAddress: %#0x\n", idd.VirtualAddress);
+        printf("\t - Size: %#0x\n", idd.Size);
+        */
     }
 
 }
 
 void parseSectionHeaders(char* fileData, int sectionHeadersOffset, int numberOfSections) {
-    printf("[SECTION-HEADERS]\n");
+    printf("\n## SECTION-HEADERS\n");
     IMAGE_SECTION_HEADER* sh = (IMAGE_SECTION_HEADER*)malloc(sizeof(IMAGE_SECTION_HEADER));
     
     for (int i = 0; i < numberOfSections; i++) {
         sh = (IMAGE_SECTION_HEADER*)&fileData[sectionHeadersOffset];
-        printf("%s\n", sh->Name);
-        printf("\tVirtual Size: %x\n", sh->Misc.VirtualSize);
-        printf("\tVirtual Address: %x\n", sh->VirtualAddress);
-        printf("\tSize Of Raw Data: %x\n", sh->SizeOfRawData);
-        printf("\tPointer To Raw Data: %x\n", sh->PointerToRawData);
-        printf("\tPointer To Relocations: %x\n", sh->PointerToRelocations);
-        printf("\tPointer To Line Numbers: %x\n", sh->PointerToLinenumbers);
-        printf("\tNumber Of Relocations: %x\n", sh->NumberOfRelocations);
-        printf("\tNumber Of Line Numbers: %x\n", sh->NumberOfLinenumbers);
-        printf("\tCharacteristics: %x\n", sh->Characteristics);
+        printf("\n| %s | |\n", sh->Name);
+        printf("| --------- | --: |\n");
+        printf("Virtual Size | %#0x\n", sh->Misc.VirtualSize);
+        printf("Virtual Address | %#0x\n", sh->VirtualAddress);
+        printf("Size Of Raw Data | %#0x\n", sh->SizeOfRawData);
+        printf("Pointer To Raw Data | %#0x\n", sh->PointerToRawData);
+        printf("Pointer To Relocations | %#0x\n", sh->PointerToRelocations);
+        printf("Pointer To Line Numbers | %#0x\n", sh->PointerToLinenumbers);
+        printf("Number Of Relocations | %#0x\n", sh->NumberOfRelocations);
+        printf("Number Of Line Numbers | %#0x\n", sh->NumberOfLinenumbers);
+        printf("Characteristics | %#0x\n", sh->Characteristics);
         printf("\n");
         sectionHeadersOffset = sectionHeadersOffset + (int)sizeof(IMAGE_SECTION_HEADER);
     }
@@ -276,9 +289,11 @@ IMAGE_SECTION_HEADER* getSection(char* fileData, int sectionHeadersOffset, int n
 void parseImportDirectory(char* fileData, IMAGE_SECTION_HEADER* importSection, int importSectionRva) {
     int rawOffset = (int)fileData + importSection->PointerToRawData;
     IMAGE_IMPORT_DESCRIPTOR* importDescriptor = (IMAGE_IMPORT_DESCRIPTOR*)(rawOffset + (importSectionRva - importSection->VirtualAddress));
-    printf("[DLL-IMPORTS]\n");
+    printf("## DLL-IMPORTS\n");
     for (; importDescriptor->Name != 0; importDescriptor++) {
-        printf("\t%s\n", rawOffset + (importDescriptor->Name - importSection->VirtualAddress)); // DLL Name
+        printf("\n| %s | |\n", rawOffset + (importDescriptor->Name - importSection->VirtualAddress)); // DLL Name
+        printf("| -- | --: |\n");
+
         int thunk;
         if (importDescriptor->OriginalFirstThunk == 0) {
             thunk = importDescriptor->FirstThunk;
@@ -290,10 +305,10 @@ void parseImportDirectory(char* fileData, IMAGE_SECTION_HEADER* importSection, i
         // DLL Functions used by this PE
         for (; thunkData->u1.AddressOfData != 0; thunkData++) {
             if (thunkData->u1.AddressOfData > 0x80000000) {
-                printf("\t\tOrdinal: %x\n", (WORD)thunkData->u1.AddressOfData);
+                printf("| Ordinal | %#0x |\n", (WORD)thunkData->u1.AddressOfData);
             }
             else {
-                printf("\t\t%s\n", (rawOffset + (thunkData->u1.AddressOfData - importSection->VirtualAddress + 2)));
+                printf("| %s | |\n", (rawOffset + (thunkData->u1.AddressOfData - importSection->VirtualAddress + 2)));
             }
         }
     }
@@ -302,20 +317,22 @@ void parseImportDirectory(char* fileData, IMAGE_SECTION_HEADER* importSection, i
 void parseExportDirectory(char* fileData, IMAGE_SECTION_HEADER* exportSection, int exportSectionRva) {
     int rawOffset = (int)fileData + exportSection->PointerToRawData;
     IMAGE_EXPORT_DIRECTORY* exportDirectory = (IMAGE_EXPORT_DIRECTORY*)(rawOffset + (exportSectionRva - exportSection->VirtualAddress));
-    printf("[DLL-EXPORTS]\n");
-    printf("\tCharacteristics: %x\n", exportDirectory->Characteristics);
-    printf("\tTimeDateStamp: %x\n", exportDirectory->TimeDateStamp);
-    printf("\tMajorVersion: %x\n", exportDirectory->MajorVersion);
-    printf("\tMinorVersion: %x\n", exportDirectory->MinorVersion);
-    printf("\tName: %x\n", exportDirectory->Name);
-    printf("\tBase: %x\n", exportDirectory->Base);
-    printf("\tNumberOfFunctions: %x\n", exportDirectory->NumberOfFunctions);
-    printf("\tNumberOfNames: %x\n", exportDirectory->NumberOfNames);
-    printf("\tAddressOfFunctions: %x\n", exportDirectory->AddressOfFunctions);
-    printf("\tAddressOfNames: %x\n", exportDirectory->AddressOfNames);
-    printf("\tAddressOfNameOrdinals: %x\n", exportDirectory->AddressOfNameOrdinals);
+    printf("\n## DLL-EXPORTS\n");
+    printf("\n| DLL-EXPORTS | |\n");
+    printf("| -- | --: |\n");
+    printf("Characteristics | %#0x\n", exportDirectory->Characteristics);
+    printf("TimeDateStamp | %#0x\n", exportDirectory->TimeDateStamp);
+    printf("MajorVersion | %#0x\n", exportDirectory->MajorVersion);
+    printf("MinorVersion | %#0x\n", exportDirectory->MinorVersion);
+    printf("Name | %#0x\n", exportDirectory->Name);
+    printf("Base | %#0x\n", exportDirectory->Base);
+    printf("NumberOfFunctions | %#0x\n", exportDirectory->NumberOfFunctions);
+    printf("NumberOfNames | %#0x\n", exportDirectory->NumberOfNames);
+    printf("AddressOfFunctions | %#0x\n", exportDirectory->AddressOfFunctions);
+    printf("AddressOfNames | %#0x\n", exportDirectory->AddressOfNames);
+    printf("AddressOfNameOrdinals | %#0x\n", exportDirectory->AddressOfNameOrdinals);
 
     int firstOffset = (int)fileData + exportDirectory->AddressOfFunctions;
-    printf("\n%x\n", (unsigned long) fileData[exportDirectory->AddressOfFunctions]);
+    printf("\n%#0x\n", (unsigned long) fileData[exportDirectory->AddressOfFunctions]);
 
 }
